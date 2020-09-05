8174ebe25b196c08eee5844a10eb2cf342fb002a
Nathan Faucett <nathanfaucett@gmail.com>
2020-09-05 16:32:03 -0400

    rebuild

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,13 @@
 export { Transform2D, Camera2D, Camera2DManager, Transform2DManager, Camera2DControl, Camera2DControlManager, Camera3D, Camera3DManager, Transform3D, Transform3DManager, Sprite, SpriteManager, SpriteClip, SpriteSheet, UIElement, UIElementManager, UIText, RunOnUpdateComponent, TransformComponent, RenderableComponent, } from "./components";
 export { Asset, Assets, JSONAsset, ImageAsset, Time, Renderer, RendererHandler, Input, InputHandler, KeyboardInputHandler, MouseInputHandler, FullScreenCanvas, InputButton, } from "./plugins";
 export { composeMat2d, decomposeMat2d, degToRad, radToDeg, DEG_TO_RAD, RAD_TO_DEG, EPSILON, TAU, HALF_PI, toHex, toRgb, toRgba, clamp, angleVec2, vec2FromAngle, getRotationFromMat2d, getAngleBetweenPoints, getAngleFromPoint, getPointFromAngle, getTangentAngle, sign, projectPointOnAxis, equals, } from "./math";
+export { Canvas } from "./Canvas";
 export { Component } from "./Component";
-export { Manager } from "./Manager";
 export { DefaultManager } from "./DefaultManager";
 export { Entity } from "./Entity";
 export { IRequirement } from "./IRequirement";
 export { Loop } from "./Loop";
-export { Scene } from "./Scene";
+export { Manager } from "./Manager";
 export { Plugin } from "./Plugin";
+export { Pool } from "./Pool";
+export { Scene } from "./Scene";
--- src/index.ts
+++ src/index.ts
@@ -60,11 +60,13 @@ export {
   projectPointOnAxis,
   equals,
 } from "./math";
+export { Canvas } from "./Canvas";
 export { Component } from "./Component";
-export { Manager } from "./Manager";
 export { DefaultManager } from "./DefaultManager";
 export { Entity } from "./Entity";
 export { IRequirement } from "./IRequirement";
 export { Loop } from "./Loop";
-export { Scene } from "./Scene";
+export { Manager } from "./Manager";
 export { Plugin } from "./Plugin";
+export { Pool } from "./Pool";
+export { Scene } from "./Scene";
